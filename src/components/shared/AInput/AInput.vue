928961c1e4d6825c1bb191fdcfa84dc711c765c3
Alex Hyriavets <alexhyriavets@gmail.com>
2019-09-18 22:04:42 +0300

    Add shared input component

---
+++ src/components/shared/AInput/AInput.vue
@@ -0,0 +1,45 @@
+<template>
+  <input :value="value" type="text" class="a-input" @input="onInput" />
+</template>
+
+<script lang="ts">
+import { createComponent } from '@vue/composition-api'
+
+export default createComponent({
+  props: {
+    value: {
+      type: [String, Number],
+      required: true
+    }
+  },
+  setup(props, context) {
+    function onInput(evt: InputEvent) {
+      if (!evt || !evt.target) return
+
+      // @ts-ignore
+      context.emit('input', evt.target.value)
+    }
+
+    return {
+      onInput
+    }
+  }
+})
+</script>
+
+<style lang="scss">
+.a-input {
+  width: 100%;
+  height: 45px;
+  border: 2px solid #e6e6e6;
+  border-radius: 7px;
+  padding: 12px 16px;
+  font-size: 16px;
+
+  &:focus {
+    border-color: #dff2e6;
+    outline: none;
+    box-shadow: 0 0 0 1px #dff2e6, 0 0 4px 0 #dff2e6;
+  }
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import VueCompositionApi from '@vue/composition-api'
 
-Vue.config.productionTip = false;
+Vue.use(VueCompositionApi)
+
+Vue.config.productionTip = false
+
+declare module '@vue/composition-api/dist/component/component' {
+  interface SetupContext {
+    readonly refs: { [key: string]: Vue | Element | Vue[] | Element[] }
+  }
+}
 
 new Vue({
   router,
-  render: (h) => h(App),
-}).$mount('#app');
+  render: h => h(App)
+}).$mount('#app')
